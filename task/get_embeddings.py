38e2c059ffb3acd07ee43280e422673d67a21ba3
Sun Hongyu <Mr.sunhy@outlook.com>
2018-10-08 11:21:16 +0800

    use 100K papers corpus to train word2vec model

--- task/get_embeddings.py
+++ task/get_embeddings.py
@@ -5,27 +5,44 @@ import io
 import jieba
 import pandas as pd
 import numpy as np
-from gensim.models import word2vec # word2vec,Word2Vec这两个包都用到了
+from gensim.models import word2vec
 from gensim.models import Word2Vec
 
 
 def tokenize():
     print('='*10, 'tokenizing', '='*10)
-    with io.open('chosen_paper.txt','r',encoding='utf-8') as content:
+    # first try
+    #with io.open('chosen_paper.txt','r',encoding='utf-8') as content:
+    # second try
+    with io.open('paper_100K.txt','r',encoding='utf-8') as content:
         for line in content:
             tokens = jieba.cut(line)
-            with io.open('vocabulary.txt', 'a', encoding='utf-8') as output:
+            # first try
+            #with io.open('vocabulary.txt', 'a', encoding='utf-8') as output:
+            # second try
+            with io.open('vocabulary_100K.txt', 'a', encoding='utf-8') as output:
                 output.write(' '.join(tokens))
     print('='*10, 'tokenizing done', '='*10)
 
 def train():
     print('='*10, 'training', '='*10)
-    num_features = 20    # Word vector dimensionality
-    min_word_count = 2   # Minimum word count
-    num_workers = 16       # Number of threads to run in parallel
+
     context = 20          # Context window size
     downsampling = 1e-3   # Downsample setting for frequent words
-    sentences = word2vec.Text8Corpus("vocabulary.txt")
+    # first try
+    #num_features = 20    # Word vector dimensionality
+    #min_word_count = 2   # Minimum word count
+    #num_workers = 16       # Number of threads to run in parallel
+    # vocabulary.txt 共包含1100篇文章
+    #sentences = word2vec.Text8Corpus("vocabulary.txt")
+
+    # second try
+    num_features = 50    # Word vector dimensionality
+    min_word_count = 10  # Minimum word count
+    num_workers = 32       # Number of threads to run in parallel
+    # 共包含10000篇文章
+    sentences = word2vec.Text8Corpus("paper_100K.txt")
+
 
     model = word2vec.Word2Vec(sentences, workers=num_workers, \
             				  size=num_features, min_count = min_word_count, \
@@ -34,14 +51,17 @@ def train():
     print('='*10, 'training done', '='*10)
 
     model.init_sims(replace=True)
-    model.save("vocabulary.model")
+    model.save("vocabulary_100K.model")
     print('='*10, 'saving model done', '='*10)
     # 可以在加载模型之后使用另外的句子来进一步训练模型
     # model = gensim.models.Word2Vec.load('/tmp/mymodel')
     # model.train(more_sentences)
 
 def get_train_data():
-    model = Word2Vec.load('vocabulary.model')
+    # first try
+    #model = Word2Vec.load('vocabulary.model')
+    # second try
+    model = Word2Vec.load('vocabulary_100K.model')
     print('neural:', model['neural'])
     
     X_train, Y_train = [], []
@@ -55,7 +75,7 @@ def get_train_data():
 
             print('>'*3, 'problem:', problem)
             problem_words = problem.split(' ')
-            x_problem = np.zeros((1, 20))
+            x_problem = np.zeros((1, 50))
             y_problem = 0
             for word in problem_words:
                 try:
@@ -72,7 +92,7 @@ def get_train_data():
 
             print('<'*3, 'method:', method)
             method_words = method.split(' ')
-            x_method = np.zeros((1, 20))
+            x_method = np.zeros((1, 50))
             y_method = 1
             for word in method_words:
                 try:
@@ -94,6 +114,6 @@ def get_train_data():
 
 if __name__ == '__main__':
     print('='*10, 'starting', '='*10, '\n')
-    tokenize()
+    #tokenize()
     train()
     print('='*10, 'ending', '='*10, '\n')
